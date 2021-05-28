bd15b267042572d728ff569213afabd4a6a5420d
joao <joaovlima07@hotmail.com>
2021-05-28 11:10:40 -0300

    cardExpense component added

--- src/components/ExpenseItem/index.js
+++ src/components/ExpenseItem/index.js
@@ -1,16 +1,17 @@
 import ExpenseDate from '../ExpenseDate/index'
 import './index.css'
+import Card from '../CardExpenses/index'
 
 function ExpenseItem(props) {
   
     return (
-    <div className="expense-item ">
+    <Card className="expense-item ">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description ">
             <h2>{props.title}</h2>
             <div className="expense-item__price">{props.amount}</div>
         </div>
-    </div>)
+    </Card>)
 }
 
 export default ExpenseItem
