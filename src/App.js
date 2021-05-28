70ea1025f838e1172aa06e107be9a76e9d4e7ce3
joao <joaovlima07@hotmail.com>
2021-05-28 11:02:03 -0300

    expenses component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from './components/ExpenseItem/index.js'
+
+import Expanses from './components/Expenses/index'
 
 function App() {
 
@@ -33,10 +34,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible</p>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <Expanses item={expenses}/>
     </div>
   );
 }
